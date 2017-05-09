e09e1c714610c75803bc9dc0a3902ad07f549271
Michael Zevin <michael.zevin@ligo.org>
2017-05-09 16:08:28 -0500

    added new argument for testing MT track, and cutting training set to be only nearby values

--- mass_transfer_GP.py
+++ mass_transfer_GP.py
@@ -34,7 +34,7 @@ print "Scikit-learn version: %s" % (sklearn.__version__)
 
 # read in arguments
 argp = argparse.ArgumentParser()
-argp.add_argument("-rp", "--resamp-path", type=str, default='/data/fine_grid/resampled/', help="Path to resampled tracks directory. Default='/data/fine_grid/resampled/'.")
+argp.add_argument("-g", "--resamp-path", type=str, default='data/fine_grid/resampled/', help="Path to resampled tracks directory. Default='/data/fine_grid/resampled/'.")
 argp.add_argument("-r", "--resamp", type=str, default='all_l2_10', help="Resampled tracks you wish to use for interpolation. Default='all_l2_10'.")
 argp.add_argument("-p", "--parameter", type=str, help="Parameter you wish to interpolate. Parameters of interest include: star_1_mass, star_2_mass, log_Teff, log_L, log_R, period_days, age, log_dt, log_abs_mdot, binary_separation, lg_mtransfer_rate, lg_mstar_dot_1, lg_mstar_dot_2, etc. Note: star_1 = companion, star_2 = black hole.")
 argp.add_argument("-t", "--test-set", type=float, default=0.2, help="Fraction of the total set that is held out for testing (i.e., the GP will be trained on the (1-t) datapoints). Default = 0.2.")
@@ -42,8 +42,7 @@ argp.add_argument("-rs", "--random-seed", type=int, help="Use this for reproduci
 argp.add_argument("-f", "--run-tag", help="Use this as the stem for all file output.")
 argp.add_argument("-S", "--save-pickle", action="store_true", help="Save the GP model as a pickle. Default is off.")
 argp.add_argument("-P", "--make-plots", action="store_true", help="Makes and saves plots. Default is off.")
-argp.add_argument("-P", "--make-plots", action="store_true", help="Makes and saves plots. Default is off.")
-argp.add_argument("-T", "--test_MT", type=int, default=0, help="Cuts the dataset to make a smaller grid centered around a testing track. Providing an integer gives the number nearest tracks you wish to keep in the training set. Default testing track is 10 Mbh, 15 M2, 2 P, 0.02 Z.")
+argp.add_argument("-T", "--test_MT", type=int, default=0, help="Cuts the dataset to make a smaller grid centered around a testing track. Providing an integer gives the number nearest tracks you wish to keep in the training set. Default testing track using this option is 10 Mbh, 15 M2, 2 P, 0.02 Z. Default path for test_MT resamplings is 'data/test_MT/resampled/<resamp>/")
 args = argp.parse_args()
 
 
@@ -64,17 +63,19 @@ param_idx = param_names.index(args.parameter)
 
 
 # read in inputs and outputs
-inputs = []
+inputs=[]
 outputs=[]
 for file in os.listdir(resamp_path):
     x = np.load(resamp_path + file)
     inputs.append((np.float(x['Mbh_init']),np.float(x['M2_init']),np.float(x['P_init']),np.float(x['Z_init'])))
     outputs.append(x['x_resamp'][:,param_idx])
 
+
 # reshape as arrays
 inputs = np.reshape(inputs,[len(inputs),4]) # 4 inputs
 outputs = np.asarray(outputs)
 resamp_len = outputs.shape[1]
+
 
 # store inputs as a dataframe
 inputs_df = pd.DataFrame({"Mbh_init": inputs[:,0], "M2_init": inputs[:,1], "P_init": inputs[:,2], "Z_init": inputs[:,3]})
@@ -85,9 +86,29 @@ print 'M2_init: %f - %f' % (inputs_df["M2_init"].min(), inputs_df["M2_init"].max
 print 'P_init: %f - %f' % (inputs_df["P_init"].min(), inputs_df["P_init"].max())
 print 'Z_init: %f - %f' % (inputs_df["Z_init"].min(), inputs_df["Z_init"].max())
 
-# normalize the inputs (all values should be positive)
-inputs = pd.DataFrame({"Mbh_init": inputs_df['Mbh_init']/inputs_df['Mbh_init'].max(),"M2_init": inputs_df['M2_init']/inputs_df['M2_init'].max(),"P_init": inputs_df['P_init']/inputs_df['P_init'].max(),"Z_init": inputs_df['Z_init']/inputs_df['Z_init'].max()})
+
+# define function to normalize inputs and normalize
+def normalize(vec, min, max):
+    return ((vec-min) / (max-min))
+def unnormalize(norm_vec, min, max):
+    return norm_vec * (max-min) / min
+inputs = pd.DataFrame({"Mbh_init": normalize(inputs_df['Mbh_init'],inputs_df['Mbh_init'].min(),inputs_df['Mbh_init'].max()),"M2_init": normalize(inputs_df['M2_init'],inputs_df['M2_init'].min(),inputs_df['M2_init'].max()),"P_init": normalize(inputs_df['P_init'],inputs_df['P_init'].min(),inputs_df['P_init'].max()),"Z_init": normalize(np.log10(inputs_df['Z_init']),np.log10(inputs_df['Z_init']).min(),np.log10(inputs_df['Z_init']).max())})
 inputs = np.array(inputs)
+
+
+# split dataset into training and testing sets
+if args.random_seed:
+    X_train_orig, X_test_orig, y_train_orig, y_test_orig = train_test_split(inputs, outputs, test_size=args.test_set, random_state=args.random_seed)
+else:
+    X_train_orig, X_test_orig, y_train_orig, y_test_orig = train_test_split(inputs, outputs, test_size=args.test_set)
+
+
+# add an extra dimension of 'step' to the normalized inputs and flatten
+step_space = np.linspace(0,resamp_len,resamp_len)/resamp_len
+X_train = np.array(list(np.append(x,y) for x in X_train_orig for y in step_space))
+X_test = np.array(list(np.append(x,y) for x in X_test_orig for y in step_space))
+y_train = y_train_orig.flatten()
+y_test = y_test_orig.flatten()
 
 
 # plot the parameter space coverage
@@ -97,6 +118,7 @@ if make_plots:
     ax.set_zlabel('$Black\ Hole\ Mass\ (M_{\odot})$', rotation=0, labelpad=20, size=12)
     ax.set_ylabel('$Companion\ Mass\ (M_{\odot})$', rotation=0, labelpad=20, size=12)
     ax.set_xlabel('$Log\ Period\ (s)$', rotation=0, labelpad=20, size=12)
+
 
     log_P = np.log10(inputs_df['P_init'])
     log_Z = np.log10(inputs_df['Z_init'])
@@ -112,20 +134,58 @@ if make_plots:
     plt.savefig(fname)
 
 
-# split dataset into training and testing sets
-if args.random_seed:
-    X_train_orig, X_test_orig, y_train_orig, y_test_orig = train_test_split(inputs, outputs, test_size=args.test_set, random_state=args.random_seed)
-else:
-    X_train_orig, X_test_orig, y_train_orig, y_test_orig = train_test_split(inputs, outputs, test_size=args.test_set)
-pdb.set_trace()
 
 
-# add an extra dimension of 'step' to the normalized inputs and flatten
-step_space = np.linspace(0,resamp_len,resamp_len)/resamp_len
-X_train = np.array(list(np.append(x,y) for x in X_train_orig for y in step_space))
-X_test = np.array(list(np.append(x,y) for x in X_test_orig for y in step_space))
-y_train = y_train_orig.flatten()
-y_test = y_test_orig.flatten()
+### If test_MT is specified, read in this track and cut the inputs tracks as specified ###
+if args.test_MT != 0:
+    testMT_path = 'data/test_MT/resampled/' + args.resamp + '/'
+    testMT_inputs=[]
+    testMT_outputs=[]
+    for file in os.listdir(testMT_path):   # should only be 1 file in this directory, but can have more
+        x = np.load(testMT_path + file)
+        testMT_inputs.append((np.float(x['Mbh_init']),np.float(x['M2_init']),np.float(x['P_init']),0.02))   # for some reason no Z recorded...
+        testMT_outputs.append(x['x_resamp'][:,param_idx])
+
+    # reshape as arrays
+    testMT_inputs = np.reshape(testMT_inputs,[len(testMT_inputs),4])
+    testMT_outputs = np.asarray(testMT_outputs)
+
+    # store testMT inputs as a dataframe
+    testMT_inputs_df = pd.DataFrame({"Mbh_init": testMT_inputs[:,0], "M2_init": testMT_inputs[:,1], "P_init": testMT_inputs[:,2], "Z_init": testMT_inputs[:,3]})
+
+    # normalize testMT inputs
+    testMT_inputs = pd.DataFrame({"Mbh_init": normalize(testMT_inputs_df['Mbh_init'],inputs_df['Mbh_init'].min(),inputs_df['Mbh_init'].max()),"M2_init": normalize(testMT_inputs_df['M2_init'],inputs_df['M2_init'].min(),inputs_df['M2_init'].max()),"P_init": normalize(testMT_inputs_df['P_init'],inputs_df['P_init'].min(),inputs_df['P_init'].max()),"Z_init": normalize(np.log10(testMT_inputs_df['Z_init']),np.log10(inputs_df['Z_init']).min(),np.log10(inputs_df['Z_init']).max())})
+    testMT_inputs = np.array(testMT_inputs)
+
+    # choose args.test_MT tracks closest to the testing track as the training data
+    norm_vecs=[]
+    for inp in inputs:
+        norm_vecs.append(np.linalg.norm(testMT_inputs-inp))
+        sorted_idxs = np.argsort(norm_vecs)
+
+    # only use the closest input and output points
+    inputs = inputs[sorted_idxs[0:args.test_MT]]
+    outputs = outputs[sorted_idxs[0:args.test_MT]]
+
+    # store inputs as a dataframe
+    inputs_df = inputs_df.iloc[sorted_idxs[0:args.test_MT]]
+    print '\nThe cut grid contains:'
+    print 'Mbh_init: %f - %f' % (inputs_df["Mbh_init"].min(), inputs_df["Mbh_init"].max())
+    print 'M2_init: %f - %f' % (inputs_df["M2_init"].min(), inputs_df["M2_init"].max())
+    print 'P_init: %f - %f' % (inputs_df["P_init"].min(), inputs_df["P_init"].max())
+    print 'Z_init: %f - %f' % (inputs_df["Z_init"].min(), inputs_df["Z_init"].max())
+
+    # split dataset into training and testing sets
+    X_train_orig, X_test_orig, y_train_orig, y_test_orig = inputs, testMT_inputs, outputs, testMT_outputs
+
+    # add an extra dimension of 'step' to the normalized inputs and flatten
+    step_space = np.linspace(0,resamp_len,resamp_len)/resamp_len
+    X_train = np.array(list(np.append(x,y) for x in X_train_orig for y in step_space))
+    X_test = np.array(list(np.append(x,y) for x in X_test_orig for y in step_space))
+    y_train = y_train_orig.flatten()
+    y_test = y_test_orig.flatten()
+
+
 
 
 ### Define GP and auxiliary functions ###
@@ -139,10 +199,9 @@ def GPR_scikit(X_train, y_train, X_test):
     c_min = np.abs(y_train).min()
     c_max = np.abs(y_train).max()
 
-    kernel = C(1e0,(c_min,c_max))*RBF(length_scale=1e-1, length_scale_bounds=(1e-6,1e0))
-    kernel = RBF(length_scale=1e-1, length_scale_bounds=(1e-3,1e0))
+    kernel = C(1e0,(1e-3,1e3))*RBF(length_scale=1e0, length_scale_bounds=(1e-6,1e6))
 
-    yerr = 1e-3
+    yerr = 1e-6
     gp = GPR(kernel=kernel, n_restarts_optimizer=9, normalize_y = True)
     gp.fit(X, y)
 
